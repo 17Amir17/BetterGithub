e87bfd2e36b6fc0ba8f7b7328431b26cc3c60ba2
17Amir17 <amirangel101@hotmail.com>
2021-12-15 14:54:40 +0200

    Added background tab change listener

--- main.js
+++ main.js
@@ -3,19 +3,46 @@ const imageDivTitleSelector = 'a.Link--primary';
 function getAllImageDivs() {
   const imageDivs = [];
   const imageReg = /[\/.](gif|jpg|jpeg|tiff|png)$/i;
-  [...document.querySelectorAll(imageDivTitleSelector)].map((elem) => {
+  const imageTitleDivs = [...document.querySelectorAll(imageDivTitleSelector)];
+  console.log(imageTitleDivs);
+  imageTitleDivs.map((elem) => {
     const elemTitle = elem.getAttribute('title');
     if (typeof elemTitle === 'string' && imageReg.test(elemTitle)) {
-      imageDivs.push(elem.parentElement.parentElement);
+      try {
+        imageDivs.push(elem.parentElement.parentElement);
+      } catch (error) {
+        console.log(error);
+      }
     }
   });
   return imageDivs;
 }
 
 function clickDiff(imageDiv) {
-  const diffButton = imageDiv.querySelectorAll('button')[2];
-  diffButton.click();
+  try {
+    const diffButton = imageDiv.querySelectorAll('button')[2];
+    diffButton.click();
+  } catch (error) {
+    console.log(error);
+  }
 }
 
-const imageDivs = getAllImageDivs();
-imageDivs.forEach(clickDiff);
+function clickAllDiff() {
+  setTimeout(() => {
+    const imageDivs = getAllImageDivs();
+    imageDivs.forEach(clickDiff);
+  }, 1000);
+}
+
+clickAllDiff();
+
+chrome.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+  // listen for messages sent from background.js
+  if (request.message === 'TAB_CHANGED') {
+    console.log(request.url.includes('files'));
+    if (request.url && request.url.endsWith('files')) {
+      console.log('Clicking!');
+      clickAllDiff();
+    }
+  }
+});
