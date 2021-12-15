ca6848117e2ca83a5b238f06698e4ddf786c531e
17Amir17 <amirangel101@hotmail.com>
2021-12-15 17:08:48 +0200

    Toggle works

--- injectDiff.js
+++ injectDiff.js
@@ -1,5 +1,9 @@
 const imageDivTitleSelector = 'a.Link--primary';
 const viewModesSelector = '.js-view-modes';
+const dataViewElemSelector = 'div[data-type="diff"]';
+const dataViewElem = document.querySelector(dataViewElemSelector);
+const diffViewElem = createDiffView();
+const diffLiElem = createDiffHtml();
 const viewMode = document.querySelector(viewModesSelector);
 
 function createDiffHtml() {
@@ -11,18 +15,37 @@ function createDiffHtml() {
   return elem;
 }
 
-function onDiffClick(e) {
-  console.log('!!');
-  const siblings = [...viewMode.querySelectorAll('li')];
-  siblings.forEach((sibling) => {
+function createDiffView() {
+  const elem = dataViewElem.querySelector('.swipe').cloneNode();
+  elem.setAttribute('class', 'difference view');
+  elem.innerText = 'DIFF ELEM';
+  return elem;
+}
+
+function deactivate(elements) {
+  elements.forEach((sibling) => {
     sibling.classList.remove('active');
   });
+}
+
+function hideAll(elements) {
+  elements.forEach((elem) => (elem.style.display = 'none'));
+}
+
+function onDiffClick(e) {
+  // Deactivate all other ticks
+  deactivate([...viewMode.querySelectorAll('li')]);
   e.target.classList.add('active');
+  // Hide all views
+  hideAll([...document.querySelectorAll('.view')]);
+  //Show diff view
+  diffViewElem.hidden = false;
 }
 
 function injectDifference() {
-  const diffElem = createDiffHtml();
-  viewMode.appendChild(diffElem);
+  viewMode.appendChild(diffLiElem);
+  //   console.log(createDiffView());
+  dataViewElem.appendChild(diffViewElem);
 }
 
 console.log('Running');
